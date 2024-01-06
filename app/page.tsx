95b3b687556716143ff0c6ccbd9c5b16e5e7af12
Baran <baranali6688@gmail.com>
2024-01-06 21:45:08 +0300

    Divider

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Reklam from "@/components/reklam";
 import Translator from "@/components/translator";
 import Image from "next/image";
 import Movies from "@/components/movies";
+import Divider from "@/components/divider";
 export default function Home() {
   return (
     <section className="flex h-full max-w-[1720px] flex-col gap-10 py-10">
@@ -10,6 +11,7 @@ export default function Home() {
       <Translator />
       <Reklam />
       <Movies />
+      <Divider />
     </section>
   );
 }
