fa4b9a7dd02f0213f5e63b7e304a3bc1393891e0
Baran <baranali6688@gmail.com>
2024-01-08 10:12:11 +0300

    Finishing Comments

--- components/comments.tsx
+++ components/comments.tsx
@@ -9,9 +9,16 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carouselc";
 import { Avatar } from "./ui/avatar";
+import movie1 from "@/public/movie1.svg";
+import Image from "next/image";
+import { FaPlay } from "react-icons/fa";
+import saveList from "@/public/savee.svg";
+import { AvatarImage } from "@radix-ui/react-avatar";
+import avatarImg from "@/public/alis.svg";
+
 export default function Comments() {
   return (
-    <section className="flex h-[350px] w-full flex-col justify-between">
+    <section className="flex h-[355px] w-full flex-col justify-between">
       <div className="flex flex-row-reverse justify-between">
         <h2 className="text-2xl">پێداچوونەوەکان</h2>
         <Link href="" className="flex items-center gap-2">
@@ -28,13 +35,45 @@ export default function Comments() {
           <CarouselContent className="gap-2">
             {Array.from({ length: 10 }).map((_, index) => (
               <CarouselItem key={index}>
-                <div className="h-full rounded-lg bg-[#212122] px-4 py-2">
-                  <div className="flex h-1/4 w-full flex-row-reverse items-center justify-between ">
+                <div className="flex h-full flex-col gap-2 rounded-lg bg-[#212122] px-4 py-2">
+                  <div className="flex h-1/5 w-full flex-row-reverse items-center justify-between border-b-2 border-[#4a4a4a]">
                     <div className="flex flex-row-reverse items-center gap-2">
-                      <Avatar />
+                      <Avatar></Avatar>
                       <div className="flex flex-col items-end leading-6">
                         <span className="m-0">ناتالیا عەزیز عەبدوڵا</span>
                         <span className="m-0">2 Stars</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="1/4 group relative  text-right ">
+                    <p className="blur-sm transition-all hover:blur-none">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Nobis ipsam tempore quaerat, culpa aliquam doloribus ipsa?
+                      Ullam quibusdam, voluptatibus aliquam tenetur obcaecati
+                      nam dignissimos soluta. Unde culpa harum enim qui?
+                    </p>
+                    <span className=" absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 font-bold transition-all group-hover:hidden">
+                      کلیك بکە بۆ بینینی سپۆیڵ
+                    </span>
+                  </div>
+                  <div className="flex h-[100px] flex-row-reverse items-end ">
+                    <Image
+                      src={movie1}
+                      alt="Great"
+                      className="h-[100px] w-[69px]"
+                    />
+                    <div className="flex h-3/4 w-full rounded-l-md bg-[#313133]">
+                      <div className="flex h-full w-2/5 items-center justify-center gap-2">
+                        <div className=" rounded-full bg-[#414143] p-4">
+                          <FaPlay />
+                        </div>
+                        <div className=" flex aspect-square items-center justify-center rounded-full bg-[#414143] p-4 px-5">
+                          <Image src={saveList} alt="savelist" />
+                        </div>
+                      </div>
+                      <div className="flex h-full w-3/5 flex-col items-end justify-center  px-5 text-right">
+                        <span>The First Soldier</span>
+                        <span>2014 / ترسناك</span>
                       </div>
                     </div>
                   </div>
---
+++ components/series.tsx
@@ -0,0 +1,76 @@
+"use client";
+import Link from "next/link";
+import { IoIosArrowBack } from "react-icons/io";
+import { CgTimer } from "react-icons/cg";
+
+import Image from "next/image";
+import { moviesList } from "@/data";
+import { FaEye } from "react-icons/fa";
+import { FaPlay } from "react-icons/fa";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import React from "react";
+export default function Series() {
+  return (
+    <section className=" flex h-full w-full flex-col justify-between gap-4">
+      <div className="flex flex-row-reverse justify-between">
+        <h2 className="text-2xl">فیلمەکان</h2>
+        <Link href="" className="flex items-center gap-2">
+          <IoIosArrowBack /> بینیبی زیاتر
+        </Link>
+      </div>
+      <div className="flex w-full gap-1">
+        {moviesList.map((movie, index) => (
+          <div
+            key={index}
+            className="flex w-full flex-col items-end justify-center "
+          >
+            <Image
+              alt=""
+              className=" h-[322px] w-[200px] rounded-xl  transition-all  group-hover:opacity-50"
+              src={movie.image}
+            />
+            <div className="absolute left-1/2 top-1/2  flex h-20 w-1/3 -translate-x-1/2 -translate-y-1/2 items-center  justify-center rounded-full bg-[#555556] opacity-0 transition-all group-hover:opacity-100">
+              <FaPlay fontSize="24px" />
+            </div>
+            <div className="absolute bottom-0 left-0 flex h-1/4 w-full flex-col px-2 opacity-0 transition-all group-hover:opacity-100 ">
+              <span className="flex gap-1">
+                {movie.rates.map((item, index) => (
+                  <span
+                    key={index}
+                    className=" flex w-full justify-start gap-2"
+                  >
+                    <Image src={item.icon} alt="rate" />
+                    {item.rate}
+                    <span className=" opacity-60">|</span>
+                  </span>
+                ))}
+              </span>
+              <span className="flex items-center gap-2">
+                <CgTimer scale={5} fontWeight={7} />
+                {movie.time} mins
+              </span>
+              <span className="flex items-center gap-1">
+                <FaEye />
+                {movie.views.toLocaleString()}
+              </span>
+            </div>
+            <p className="flex flex-col text-right">
+              <span>{movie.name}</span>
+              <span className="text-[#888889]">
+                {movie.year} / {movie.genre}
+              </span>
+            </p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
